7355193ae0c88a5eca5aecedc53e468e5c2dbecb
quan <quan_xiaoxiao@qq.com>
2025-03-28 09:28:46 +0800

    xxx

--- src/generateID.mjs
+++ src/generateID.mjs
@@ -1,12 +1,17 @@
 import assert from 'node:assert';
 
-export default (max = 2147483647) => {
+export default (startAt = 0, max = 2147483647) => {
   assert(typeof max === 'number' && Number.isInteger(max) && max > 1, 'max must be an integer greater than 1');
 
-  let current = 0;
+  let current = startAt || 0;
+
+  if (current >= max) {
+    current = 0;
+  }
 
   return () => {
-    current = (current % max) + 1;
-    return current;
+    const _id = current;
+    current = (current + 1) % max;
+    return _id;
   };
 };
--- src/generateID.test.mjs
+++ src/generateID.test.mjs
@@ -5,14 +5,18 @@ import generateID from './generateID.mjs';
 
 test('generateID', () => {
   assert.throws(() => {
-    generateID(0);
+    generateID(0, 0);
   });
   assert.throws(() => {
-    generateID(1);
+    generateID(0, 1);
   });
   const count = 10;
-  const _id = generateID(count);
+  const _id = generateID(0, count);
   for (let i = 0; i < 1003; i++) {
-    assert.equal(_id(), (i % count) + 1);
+    assert.equal(_id(), (i % count));
   }
+
+  assert.equal(generateID(5)(), 5);
+  assert.equal(generateID(9)(), 9);
+  assert.equal(generateID(9, 5)(), 0);
 });
